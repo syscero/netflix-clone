19e82449dec0947a57211d08313769ee3ec7663b
Cícero Silva <syscero@gmail.com>
2021-07-07 22:50:38 -0300

    Versao inicial

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        1500:{
+            items:8
+        },
+        1900:{
+            items:9
+        }
+    }
+})
